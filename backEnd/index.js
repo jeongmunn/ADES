577dfa6c55b1a04b9853e2305a25280cb18cfea1
rameshMonika <Monika.20@ichat.sp.edu.sg>
2021-10-30 23:10:42 +0800

    backend base done need to check

--- backEnd/index.js
+++ backEnd/index.js
@@ -12,50 +12,9 @@ const port = 3000;
 //imports
 //---------------------------
 const express=require("express");
-const app =express();
+const app = require('./controllers/app.js');
 
 
-
-
-/** 
- * @param {object} req 
- *  request object
- * @param {object} res  
- *  response object 
- * @param {function} 
- *  reference to the enxt function to call
- * 
- */
-
-
- function printDebugInfo(req, res, next) {
-    console.log();
-    console.log("-----------------[Debug Info]----------");
-    //console.log(`Servicing ${urlPattern}...`);
-    console.log("Servicing" + req.url + " ..");
-
-    console.log("> req.params:" + JSON.stringify(req.params));
-    console.log("> req.body:" + JSON.stringify(req.body));
-    // console.log("> req.myOwnDebugIssue:"+JSON.stringify(req.myOwnDebugInfo));
-    console.log("-----------------[Debug Info Ends]----------");
-    console.log();
-
-    next();
-
-}
-
-
-app.get("/async", async (req, res) => {
-	try {
-		const response = await axios({
-			url: "users",
-			method: "get",
-		});
-		res.status(200).json(response.data);
-	} catch (err) {
-		res.status(500).json({ message: err });
-	}
-});
 
 
 //standard for express
