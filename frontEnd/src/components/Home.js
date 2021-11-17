38deed0c40c74ca65425f3e2002d76ae19bef423
17-isaac <isaa.71717@gmail.com>
2021-11-18 02:45:33 +0800

    Importing of Font works.

--- frontEnd/src/components/Home.js
+++ frontEnd/src/components/Home.js
@@ -1,32 +1,32 @@
-import logo from '../logo.svg';
-import '../App.css';
-import React from 'react'
-import { auth } from '../firebase.js';
-import { useNavigate } from 'react-router-dom';
-import {
-    onAuthStateChanged,
-    signOut
-} from "firebase/auth";
+// import logo from '../logo.svg';
+// import '../App.css';
+// import React from 'react'
+// import { auth } from '../firebase.js';
+// import { useNavigate } from 'react-router-dom';
+// import {
+//     onAuthStateChanged,
+//     signOut
+// } from "firebase/auth";
 
-const Home = () => {
-    let navigate = useNavigate();
-    const logout = async () => {
-        await signOut(auth);
-        navigate('/');
-    };
-    // const user = auth.currentUser;
-    // console.log(JSON.stringify(user));
-    return (
-        <div>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-                <button onClick={logout}>Sign Out</button>
-            </header>
-        </div>
-    )
-}
-export default Home;
+// const Home = () => {
+//     let navigate = useNavigate();
+//     const logout = async () => {
+//         await signOut(auth);
+//         navigate('/');
+//     };
+//     // const user = auth.currentUser;
+//     // console.log(JSON.stringify(user));
+//     return (
+//         <div>
+//             <header className="App-header">
+//                 <img src={logo} className="App-logo" alt="logo" />
+//                 <p>
+//                     Edit <code>src/App.js</code> and save to reload.
+//                 </p>
+//                 <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
+//                 <button onClick={logout}>Sign Out</button>
+//             </header>
+//         </div>
+//     )
+// }
+// export default Home;
