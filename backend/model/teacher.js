b6a2b983a7b4085759a1f1636eb322eab0cd2c18
17-isaac <isaa.71717@gmail.com>
2021-11-18 16:03:05 +0800

    teacher.js integrate

---
+++ backend/model/teacher.js
@@ -0,0 +1,37 @@
+console.log("---------------------------------------------------------");
+console.log("ADES> model >teacher.js");
+console.log("---------------------------------------------------------");
+
+
+const pool = require("../controllers/dbconfig")
+
+// ----------------------------------------------------------------------------
+// Objects/functions
+// ----------------------------------------------------------------------------
+
+
+var teacherDB = {
+    getStudentProgress: function (callback) {
+      console.log("function CALLED---------")
+      const sql = `SELECT "public"."Student"."studentID","public"."Student"."name", "public"."Student"."streaks", "public"."Student"."mazeLvl","public"."Student"."totalPts"
+      FROM "public"."Student";`;
+
+        pool.query(sql, (err, result) => {
+          if (err) {
+            console.log(err);
+            return callback(err.null);
+        } else {
+            console.log("its over HEREEEE")
+            return callback(null, result);
+        }
+           // pool.end()
+          })
+          
+    },
+
+};
+
+// ----------------------------------------------------------------------------
+// exports
+// ----------------------------------------------------------------------------
+module.exports = teacherDB;
--- frontEnd/src/components/RewardAdmin.js
+++ frontEnd/src/components/RewardAdmin.js
@@ -1,48 +1,134 @@
 import React from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import Button from 'react-bootstrap/Button';
+import '../css/rewardAdmin.css';
 
-export default class RewardAdmin extends React.Component {
+export default class viewReward extends React.Component {
     state = {
-        data: []
+        data: [],
+        rewardName: '',
+        rewardPoints: '',
+        url: ''
     }
 
-    componentDidMount(){
-        axios.get('https://ades-ca1-project.herokuapp.com/api/rewards')
-        .then(res => {
-            this.setState({ data : res.data });
+    componentDidMount() {
+        //axios.get('https://ades-ca1-heroku.herokuapp.com/api/rewards')
+        axios.get('https://ades-ca1-heroku.herokuapp.com/api/rewards')
+            .then(res => {
+                this.setState({ data: res.data });
+            })
+    }
+
+    handleDelete = event => {
+        const id = event.target.id;
+        axios.delete('https://ades-ca1-heroku.herokuapp.com/api/rewards/' + id)
+            .then(res => {
+                window.alert("Reward deleted successfully");
+                window.location.reload();
+            })
+    }
+
+    handleName = event => {
+        this.setState({ rewardName: event.target.value });
+    }
+
+    handlePoints = event => {
+        this.setState({ rewardPoints: event.target.value });
+    }
+
+    handleURL = event => {
+        this.setState({ url: event.target.files[0], });
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+        // file upload
+        // packing data    
+        const storage = getStorage();
+        const storageRef = ref(storage, 'img/' + this.state.url.name);
+        var file = this.state.url;
+        // Create file metadata including the content type
+        /** @type {any} */
+        const metadata = {
+            contentType: this.state.url.type,
+        };
+        uploadBytes(storageRef, file, metadata);
+
+        getDownloadURL(storageRef).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+
+            const reward = {
+                rewardName: this.state.rewardName,
+                ptsRequired: this.state.rewardPoints,
+                url: downloadURL
+            };
+            console.log("reward" + JSON.stringify(reward))
+
+            const config = {
+                headers: {
+                    'content-type': 'application/json'
+                }
+            }
+
+            //axios.post('https://ades-ca1-heroku.herokuapp.com/api/rewards', reward, config )
+            axios.post('https://ades-ca1-heroku.herokuapp.com/api/rewards', reward, config)
+                .then(res => {
+                    console.log(res);
+                    console.log(res.data);
+                    window.location.reload();
+                })
         })
     }
 
-    
     render() {
-        const data = this.state.data ;
+        const data = this.state.data;
         return (
-            <div className="viewReward">
-            <h3 className="p-3 text-center">React - Display a list of items</h3>
-            <Link to="/uploadReward">
-                <Button>Upload Reward</Button>
-            </Link>
-            <table className="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Points</th>
-                        <th>Picture</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data && data.map(item =>
-                        <tr key={item.rewardID}>
-                            <td>{item.rewardName}</td>
-                            <td>{item.ptsRequired}</td>
-                            <td><img src={'../images/'+ item.url} style={{height: 200, width: 200}}></img></td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-          </div>
+            <div id="body">
+                <div id="addReward">
+                    <h1>Reward Administration</h1>
+                    <div id="divForm">
+                        {/* <h2>Add Reward</h2> */}
+                        <form onSubmit={this.handleSubmit} id="form">
+                            <label>
+                                Reward Name :
+            <input type="text" name="rewardName" onChange={this.handleName} />
+                            </label>
+                            <label>
+                                Points Required :
+            <input type="number" name="ptsRequired" onChange={this.handlePoints} />
+                            </label>
+                            <label>
+                                Picture URL :
+            <input type="file" name="url" onChange={this.handleURL} />
+                            </label>
+                            <Button type="submit">Add</Button>
+                        </form>
+                    </div>
+                    <div id="viewRewards">
+                        {/* <h2>View Rewards</h2> */}
+                        <table class="table">
+                            <tr>
+                                <th>Name</th>
+                                <th>Points</th>
+                                <th>Picture</th>
+                                <th>Edit?</th>
+                                <th>Delete?</th>
+                            </tr>
+                            {data && data.map(item =>
+                                <tr key={item.rewardID}>
+                                    <td>{item.rewardName}</td>
+                                    <td>{item.ptsRequired}</td>
+                                    <td><img src={item.url} style={{ height: 230, width: 230 }}></img></td>
+                                    <td><button type="button" class="btn btn-outline-warning">Edit</button></td>
+                                    <td><button type="button" class="btn btn-outline-danger" id={item.rewardID} onClick={this.handleDelete}>Delete</button></td>
+                                </tr>
+                            )}
+                        </table>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
