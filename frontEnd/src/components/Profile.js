8936df9ec468d1a2cc46d79f1c215b69322a4bf3
17-isaac <isaa.71717@gmail.com>
2021-11-24 13:14:11 +0800

    changed something in profile

--- frontEnd/src/components/Profile.js
+++ frontEnd/src/components/Profile.js
@@ -90,7 +90,7 @@ export default function Profile() {
                 label="Email"
                 margin="dense"
                 type="email"
-                value={emailData}
+                placeholder={emailData}
                 onChange={handleChange}
             />
             <Button sx={{ background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)', color: 'white', paddingTop: '10px', paddingBottom: '10px', margin: '10px', width: '100%' }} variant="contained" onClick={updateEmailFunction}> Update Email</Button>
