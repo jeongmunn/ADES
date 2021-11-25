66d72e79e9dea329e3098b634efd6af087d18fea
JingWen <jeongmunn@gmail.com>
2021-11-25 20:09:45 +0800

    Update user.js

--- backend/model/user.js
+++ backend/model/user.js
@@ -5,42 +5,45 @@ console.log("---------------------------------------------------------");
 const pool = require("../controllers/dbconfig");
 
 var user = {
-    getIdAndTypeOfUser: function (Uid, callback) {
-        const sql = `SELECT "studentID", "type" FROM public."Student" WHERE "Uid" = $1`;
-        pool.query(sql, [Uid], (err, result) => {
-            if (err) {
-                console.log(err);
-                return callback(err);
-            } else {
-                return callback(null, result);
-            }
-        })
-    },
-    getEmail: function (Uid, callback) {
-        const sql = `SELECT "name" FROM public."Student" WHERE "Uid" = $1`;
-        pool.query(sql, [Uid], (err, result) => {
-            if (err) {
-                console.log(err);
-                return callback(err);
-            } else {
-                return callback(null, result);
-            }
-        })
-    },
-    updateEmail: function (Uid, email, callback) {
-        const value = [Uid, email];
-        const sql = `UPDATE public."Student" SET "name"= $2 WHERE "Uid" = $1`;
-        pool.query(sql, value, (err, result) => {
-            if (err) {
-                console.log(err);
-                return callback(err);
-            } else {
-                return callback(null, result);
-            }
-        })
+    var user = {
+        getIdAndTypeOfUser: function (Uid, callback) {
+            console.log("get id & type of user");
+            const sql = `SELECT "studentID", "type" FROM public."Student" WHERE "Uid" = $1`;
+            pool.query(sql, [Uid], (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return callback(err.null);
+                } else {
+                    return callback(null, result.rows);
+                }
+            })
+        },
+        getEmail: function (Uid, callback) {
+            console.log("get email of user");
+            const sql = `SELECT "name" FROM public."Student" WHERE "Uid" = $1`;
+            pool.query(sql, [Uid], (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return callback(err.null);
+                } else {
+                    return callback(null, result.rows);
+                }
+            })
+        },
+        updateEmail: function (Uid, email, callback) {
+            console.log("get email of user");
+            const value = [Uid, email];
+            const sql = `UPDATE public."Student" SET "name"= $2 WHERE "Uid" = $1`;
+            pool.query(sql, value, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return callback(err.null);
+                } else {
+                    return callback(null, result.rows);
+                }
+            })
+        }
     }
-}
-
 // ----------------------------------------------------------------------------
 // exports
 // ----------------------------------------------------------------------------
